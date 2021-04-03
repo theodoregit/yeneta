25ae72d6b974f2fde818b2c4b5081d7d1f150e18
mark <markonaleco@gmail.com>
2021-04-03 05:14:00 -0800

    col-lg-9 col-lg-offset-6 text-center

--- resources/views/yeneta/registrar/home.blade.php
+++ resources/views/yeneta/registrar/home.blade.php
@@ -142,8 +142,8 @@
             @endforeach
                   @else
                   <h3><p><b><i> <p style="color: black">First please add a Todo</b></i></p></h3>
-                  @endif
-                  <div class="col-lg-6 col-lg-offset-3 text-center">
+                 @endif
+                    <div class="col-lg-9 col-lg-offset-6 text-center">
                     <form action="{{route('registrar.store.todo')}}" method="POST">
                         {{ csrf_field() }}
                         <input type="text" class="form-control" name="todo" placeholder="Create a new Todo">
